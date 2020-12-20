b11c3c99fecaab38a1fd7c659217f096ebcadce5
LeeJangHee <56954702+LeeJangHee@users.noreply.github.com>
2020-12-21 01:02:29 +0900

    20201220 Update 1. Retrofit2 서비스 구현 2. 로그인 백그라운드 그라데이션

---
+++ app/src/main/java/com/example/dhu_timetable/service/APIService.java
@@ -0,0 +1,7 @@
+package com.example.dhu_timetable.service;
+
+import retrofit2.http.GET;
+
+public interface APIService {
+
+}
---
+++ app/src/main/java/com/example/dhu_timetable/service/RetrofitConnect.java
@@ -0,0 +1,20 @@
+package com.example.dhu_timetable.service;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitConnect {
+    private static String baseURL = "wkdgml96.iptime.org:8080/timetable";
+    private static Retrofit retrofit;
+
+    public static Retrofit getRetrofitClient() {
+        if (retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(baseURL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+
+        return retrofit;
+    }
+}
