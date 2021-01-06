b15e6ddc61666ab08acdd28924bbba336a62ae01
kangjaemingit <71342315+kangjaemingit@users.noreply.github.com>
2021-01-07 00:32:04 +0900

    앱바 수정, 검색화면 구현

--- app/src/main/java/com/example/dhu_timetable/MainActivity.java
+++ app/src/main/java/com/example/dhu_timetable/MainActivity.java
@@ -1,13 +1,21 @@
 package com.example.dhu_timetable;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.StringRes;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.viewpager2.widget.ViewPager2;
 
 import com.example.dhu_timetable.ui.main.SectionsPagerAdapter;
+import com.example.dhu_timetable.ui.search.SearchActivity;
+import com.google.android.material.appbar.AppBarLayout;
+import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
 
@@ -17,6 +25,9 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
     @StringRes
     private List<Integer> tabTitle = Arrays.asList(R.string.tab_text_1, R.string. tab_text_2);
+    AppBarLayout topAppBar;
+    MaterialToolbar toolbar;
+    MenuItem Search;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +35,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, tabTitle.size());
+        topAppBar = findViewById(R.id.topappbar);
+        toolbar = (MaterialToolbar)findViewById(R.id.toolbar);
 
         // 뷰 스와이프 기능
         ViewPager2 viewPager2 = findViewById(R.id.view_pager);
@@ -38,5 +51,18 @@ public class MainActivity extends AppCompatActivity {
                         tab.setText(getResources().getString(tabTitle.get(position)));
                     }
                 }).attach();
+
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                if(item.getItemId() == R.id.item_search){
+                    Intent it = new Intent(MainActivity.this, SearchActivity.class);
+                    startActivity(it);
+                }
+
+                return false;
+            }
+        });
     }
+
 }
---
+++ app/src/main/java/com/example/dhu_timetable/ui/search/SearchActivity.java
@@ -0,0 +1,17 @@
+package com.example.dhu_timetable.ui.search;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.dhu_timetable.R;
+
+public class SearchActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+
+    }
+}
